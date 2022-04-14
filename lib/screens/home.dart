afe09f613d178ab40e01e32e379e0cabda588365
Einsam <snitxmhm@gmail.com>
2022-04-14 17:40:23 +0800

    底部状态栏选项状态可变

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,11 +39,26 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class _BottomNavigationBar extends StatelessWidget {
+class _BottomNavigationBar extends StatefulWidget {
   const _BottomNavigationBar({Key? key, required this.onItemSelected})
       : super(key: key);
 
   final ValueChanged<int> onItemSelected;
+
+  @override
+  __BottomNavigationBarState createState() => __BottomNavigationBarState();
+}
+
+class __BottomNavigationBarState extends State<_BottomNavigationBar> {
+  var selectedIndex = 0;
+
+  void handleItemSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    widget.onItemSelected(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -55,27 +70,31 @@ class _BottomNavigationBar extends StatelessWidget {
         children: [
           _NavigationBarItem(
             index: 0,
+            isSelected: (selectedIndex == 0),
             lable: 'Messages',
             icon: CupertinoIcons.bubble_left_bubble_right_fill,
-            onTap: onItemSelected,
+            onTap: handleItemSelected,
           ),
           _NavigationBarItem(
             index: 1,
+            isSelected: (selectedIndex == 1),
             lable: 'Notifications',
             icon: CupertinoIcons.bell_solid,
-            onTap: onItemSelected,
+            onTap: handleItemSelected,
           ),
           _NavigationBarItem(
             index: 2,
+            isSelected: (selectedIndex == 2),
             lable: 'Calls',
             icon: CupertinoIcons.phone_fill,
-            onTap: onItemSelected,
+            onTap: handleItemSelected,
           ),
           _NavigationBarItem(
             index: 3,
+            isSelected: (selectedIndex == 3),
             lable: 'Contacts',
             icon: CupertinoIcons.person_2_fill,
-            onTap: onItemSelected,
+            onTap: handleItemSelected,
           ),
         ],
       ),
@@ -87,12 +106,14 @@ class _NavigationBarItem extends StatelessWidget {
   const _NavigationBarItem({
     Key? key,
     required this.index,
+    this.isSelected = false,
     required this.lable,
     required this.icon,
     required this.onTap,
   }) : super(key: key);
 
   final int index;
+  final bool isSelected;
   final String lable;
   final IconData icon;
   final ValueChanged<int> onTap;
@@ -112,10 +133,13 @@ class _NavigationBarItem extends StatelessWidget {
               Icon(
                 icon,
                 size: 20,
+                color: isSelected ? AppColors.secondary : null,
               ),
               Text(
                 lable,
-                style: const TextStyle(fontSize: 12),
+                style: isSelected
+                    ? const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: AppColors.secondary)
+                    : const TextStyle(fontSize: 12),
               ),
             ],
           ),
